87c959c1c69f254cc79cb00dbba93516205a1dc0
flobeira <flobeira@sandiego.edu>
2020-06-09 19:27:02 -0700

    Final

--- Leaflet-Step-1/js/logic.js
+++ Leaflet-Step-1/js/logic.js
@@ -11,7 +11,7 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox/streets-v11",
-    accessToken: pk.eyJ1IjoiZmxvYmVpcmEiLCJhIjoiY2thb29pYzFhMDR2OTJ3cWlrbXNqaDN4cCJ9.Qwstu7yDKcCm-_XdQEzfxg
+    accessToken: 
   });
 
 // Store API quary Variables 
